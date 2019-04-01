82b1ed8bedaeb7145055ec9acf09414c2e6778c3
grace-ko <gracekodes@gmail.com>
2019-03-31 20:51:49 -0700

    completed intro to express assignment

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.send("Welcome to Blogit");
 });
+router.get("/marco", (req, res, next) => {
+  res.send("polo");
+});
 
 module.exports = router;
