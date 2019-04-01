e48fa9dd5977a3ff0ee4680581f8f870c3242523
grace-ko <gracekodes@gmail.com>
2019-04-01 06:34:36 -0700

    completed routing assignment

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,8 @@
 module.exports = {
   index(req, res, next){
     res.render("static/index", {title: "Welcome to Blogit"});
+  },
+  about(req, res, next){
+    res.render("static/partials/about", {title: "Welcome to Blogit"});
   }
 }
