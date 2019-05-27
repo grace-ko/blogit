47966f9285edc8cd2dab36ea8d210f0452a7ac28
grace-ko <gracekodes@gmail.com>
2019-05-27 01:43:19 -0700

    completed comment checkpoint

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -29,13 +29,11 @@ module.exports = {
     }
   },
   validateTopics(req, res, next) {
-
    if(req.method === "POST") {
 
      req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
      req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
    }
-
    const errors = req.validationErrors();
 
    if (errors) {
@@ -44,5 +42,17 @@ module.exports = {
    } else {
      return next();
    }
- }
+ },
+ validateComments(req, res, next) {
+    if(req.method === "POST") {
+      req.checkBody("body", "must not be empty"). notEmpty();
+    }
+    const errors = req.validationErrors();
+    if (errors) {
+      req.flash("error", errors);
+      return res.redirect(req.headers.referer);
+    } else {
+      return next()
+    }
+  }
 }
